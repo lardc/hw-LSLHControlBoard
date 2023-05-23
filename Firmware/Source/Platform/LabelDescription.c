45034a602663c03fce096d43b22c48be29352ad2
asytyi <alexander.a.sytyi@gmail.com>
2023-05-23 19:47:30 +0300

    Реализована работа с меткой

---
+++ Firmware/Source/Platform/LabelDescription.c
@@ -0,0 +1,9 @@
+﻿// Header
+#include "LabelDescription.h"
+
+// Variables
+const Label BoardLabels[] =
+{
+		{"LSLControlBoard v.1.2 [Manufacturing]",	SID_PCB1_2_Manuf}
+};
+const Int16U BoardLabelsSize = sizeof(BoardLabels) / sizeof(BoardLabels[0]);
---
+++ Firmware/Source/Platform/LabelIndexSelector.h
@@ -0,0 +1,14 @@
+﻿#ifndef __LABEL_INDEX_SELECTOR_H
+#define __LABEL_INDEX_SELECTOR_H
+
+// Include
+#include "stdinc.h"
+
+// Types
+typedef enum __SelectorIndex
+{
+	SID_None = -1,
+	SID_PCB1_2_Manuf = 0
+} SelectorIndex;
+
+#endif // __LABEL_INDEX_SELECTOR_H
