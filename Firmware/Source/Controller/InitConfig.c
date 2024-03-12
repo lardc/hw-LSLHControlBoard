3168c89fd17da05041e48c5c743fb9c57959e84a
mlhzv2204 <r.malkhozov@proton-electrotex.com>
2024-03-12 17:50:15 +0300

    Сэмпл-тайм изменен на ADC_SMPL_TIME_61_5

--- Firmware/Source/Controller/InitConfig.c
+++ Firmware/Source/Controller/InitConfig.c
@@ -90,8 +90,8 @@ void INITCFG_ConfigADC()
 	ADC_ChannelSeqLen(ADC1, 2);
 	ADC_ChannelSet_Sequence(ADC1, ADC1_ID_CHANNEL, 1);
 	ADC_ChannelSet_Sequence(ADC1, ADC1_VD_CHANNEL, 2);
-	ADC_ChannelSet_SampleTime(ADC1, ADC1_ID_CHANNEL, ADC_SMPL_TIME_7_5);
-	ADC_ChannelSet_SampleTime(ADC1, ADC1_VD_CHANNEL, ADC_SMPL_TIME_7_5);
+	ADC_ChannelSet_SampleTime(ADC1, ADC1_ID_CHANNEL, ADC_SMPL_TIME_61_5);
+	ADC_ChannelSet_SampleTime(ADC1, ADC1_VD_CHANNEL, ADC_SMPL_TIME_61_5);
 	ADC_TrigConfig(ADC1, ADC12_TIM1_TRGO, RISE);
 
 	ADC_DMAConfig(ADC1);
